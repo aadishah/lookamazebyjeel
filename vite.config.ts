36360a2512fe3cd67ac45bdaa640c5e939618bb4
Aaditya Shah <adityashah.06@gmail.com>
2026-04-16 13:54:03 +0530

    Update nitro import path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
-import nitro from 'nitro/dist/app'
+import nitro from 'nitro'
 
 export default defineConfig({
   plugins: [
